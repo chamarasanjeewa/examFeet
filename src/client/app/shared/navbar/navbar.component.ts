b2943150571e1d4d0fb8b5b406ceb1118b182774
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2017-01-12 19:59:06 +0530

    changes to toggle the menu bar

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['navbar.component.css'],
 })
 
-export class NavbarComponent { }
+export class NavbarComponent {
+    menuVisible:boolean;
+    toggle(){
+        this.menuVisible=!this.menuVisible;
+    }
+ }
