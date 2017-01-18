9fbacbb11b6eb58e6e21268480e800b0f673b4b5
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2017-01-18 20:09:16 +0530

    added changes

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'sd-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
+   host: {'class': 'home'}
 })
 
 export class HomeComponent implements OnInit {
